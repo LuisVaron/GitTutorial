4bb633704513031ca2baba61ba70c9b388138756
Aulas Informáticas Universidad El Bosque <aulainformatica@unbosque.edu.co>
2022-02-11 12:27:50 -0500

    Se agrega clase Student y linea en Applicaiton para leer nombre y definir dentro de clase Student

--- src/edu/unbosque/gittutorial/Application.java
+++ src/edu/unbosque/gittutorial/Application.java
@@ -1,8 +1,18 @@
 package edu.unbosque.gittutorial;
+import java.util.Scanner;
 
 public class Application {
 
     public static void main (String[] args){
+        
+        Student Student = new Student(null);
+        
         System.out.println("Bienvenido estudiante");
+        System.out.println("Ingrese el nombre del estudiante");
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+
+        Student.setName(name);
     }
 }
